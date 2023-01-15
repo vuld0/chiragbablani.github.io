35520ca706b36dc1e39af1463b59fd8c8b97ce7a
vuld0 <chiragsonubablani@karunya.edu.in>
2023-01-15 22:49:32 +0530

    Add CDN

--- next.config.js
+++ next.config.js
@@ -8,4 +8,11 @@ const nextConfig = {
   swcMinify: true,
 };
 
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/vuld0/chiragbablani.github.io/gh-pages/' : '',
+}
+
 module.exports = nextConfig;
